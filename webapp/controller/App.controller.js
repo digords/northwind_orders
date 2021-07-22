5dc53f22f33aafb4dcaaf89cb0f790f822a1b65a
digords <digords@gmail.com>
2021-07-22 11:28:20 -0300

    Initial development

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("dig.demo.northwind.orders.orders.controller.App", {
+			onInit: function () {
+
+			}
+		});
+	});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"digdemo.northwind.orders./orders/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
